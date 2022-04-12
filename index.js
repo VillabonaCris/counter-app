a14c7bb62e70940389ef051d85e225124a334b96
Cristian Villabona <cristianvillabonabr@gmail.com>
2022-04-12 17:49:25 -0300

    buttons and events

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 //document.getElementById('count-el').innerText = 5;
 let countDisplay = document.getElementById('count-el');
 let increaseButton = document.getElementById('increase')
+let decreaseButton = document.getElementById('decrease');
 let count = 0;
+let save = document.getElementById('save-btn');
+let p = document.getElementById('prev-entries');
 
 increaseButton.addEventListener('click', () => {
   countDisplay.innerText = count += 1;
+})
+
+decreaseButton.addEventListener('click', () => {
+  countDisplay.innerText = count -= 1;
+})
+
+save.addEventListener('click', () => {
+  let countStr = ` ${count + ' - '}`;
+  p.innerText += countStr;
+  count = 0;
+  countDisplay.innerText = count;
+
 })
 
 let myAge = 29;
